81d7d7cda8d38aea2953b423d2021717c465303e
krahulp92-lgtm <krahulp92@gmail.com>
2026-09-09 11:39:58 +0530

    Update dashboard (1).py

--- dashboard (1).py
+++ dashboard (1).py
@@ -489,7 +489,7 @@ def angel_login():
     # SEND THE CURRENT 6-DIGIT OTP,
     # NOT ANGEL_TOTP_SECRET
     # --------------------------------------------------------
-  totp = pyotp.TOTP(secret).now()
+    totp = pyotp.TOTP(secret).now()
 
   try:
 
